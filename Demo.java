61ece5f94400f543aa24e5ef36cd46297784831f
CJC <sammedkoshti7@gmail.com>
2026-04-09 16:47:46 +0530

    login test method added

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ public class Demo{
     public static void main(String[] args)
     {
         System.out.println("this is first time demo.java created");
+        System.out.println("login test method added");
     }
 }
